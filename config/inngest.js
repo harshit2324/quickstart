cef54acc4499aa7401d3975af34bfed06ddde5a9
harshit2324 <richhariyaharshit684@gmail.com>
2025-08-17 01:42:22 +0530

    fixed: issue

--- config/inngest.js
+++ config/inngest.js
@@ -34,7 +34,7 @@ export const syncUserUpdation = inngest.createFunction(
           const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData ={
             _id: id,
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image_url: image_url
         }
